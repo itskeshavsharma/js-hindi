20427ed01e25c406f72be0ac33c58f25d9904e67
Keshav <keshavsharma7167@gmail.com>
2025-03-08 19:43:57 +0530

    Initial commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Keshav"
+const repocount = 50
+
+//console.log(name + " "+ repocount +" "+ "value")
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+const gameName = new String('Keshav-ks-com')
+console.log(gameName[4]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+
